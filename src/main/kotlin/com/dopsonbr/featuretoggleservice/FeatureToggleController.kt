e35fc3e1d8400258bfa58a66623b1109086aef83
SVC_CI_COM <svc_ci_com@homedepot.com>
2018-01-07 20:52:23 -0500

    can connect to mysql db

---
+++ src/main/kotlin/com/dopsonbr/featuretoggleservice/FeatureToggleController.kt
@@ -0,0 +1,74 @@
+package com.dopsonbr.featuretoggleservice
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.Repository
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+@RestController
+class FeatureToggleController {
+
+    @Autowired
+    lateinit var storeFeatureToggleRepository: storeFeatureToggleRepository
+
+    @Autowired
+    lateinit var storeFeatureToggleRepository2: storeFeatureToggleRepository2
+
+    @CrossOrigin
+    @GetMapping(value = ["/featureToggles"])
+    fun fetchFeatureToggles(@RequestParam storeNumber: String = "9798",
+                            @RequestParam environment: String = "development",
+                            @RequestParam productTeam: String = "sell"):
+            List<String>? {
+        val toggles: MutableIterable<StoreFeatureToggle>? = storeFeatureToggleRepository2
+                .findByStoreNumberAndEnvironmentAndProductTeam(storeNumber, environment, productTeam)
+        return toggles?.map { it.feature }
+
+    }
+
+    @PutMapping(path = ["/featureToggles"])
+    @ResponseBody
+    fun addNewUser(@RequestParam storeNumber: String,
+                   @RequestParam environment: String,
+                   @RequestParam productTeam: String,
+                   @RequestParam feature: String): String {
+
+        val n = StoreFeatureToggle()
+        n.environment = environment
+        n.productTeam = productTeam
+        n.feature = feature
+        n.storeNumber = storeNumber
+        storeFeatureToggleRepository.save(n)
+        return "Saved"
+    }
+}
+
+interface storeFeatureToggleRepository : CrudRepository<StoreFeatureToggle, Long>
+
+
+interface storeFeatureToggleRepository2 : Repository<StoreFeatureToggle, Long> {
+    fun findByStoreNumberAndEnvironmentAndProductTeam(storeNumber: String,
+                                                      environment: String,
+                                                      productTeam: String):MutableIterable<StoreFeatureToggle>?
+}
+
+@Entity
+class StoreFeatureToggle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+    var storeNumber: String = ""
+    var environment: String = ""
+    var productTeam: String = ""
+    var feature: String = ""
+}
